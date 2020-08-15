25a94dbdf9c2e10de6711bb64debbf0dc74fcfe1
Alison <alison.a.hernandez@gmail.com>
2020-08-14 22:39:46 -0400

    finished cycle 1

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,29 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+Student.beforeValidate((student) => {
+  const capitalFirst = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
+  // console.log(capitalFirst)
+  student.firstName = capitalFirst;
+  student.lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
+})
 
 module.exports = Student;
